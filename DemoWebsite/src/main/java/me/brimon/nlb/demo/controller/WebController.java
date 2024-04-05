0759dd77aa5ec0431ddaf5cf71fe44607c71dcb9
古城秋月 <acgucheng@gmail.com>
2024-04-05 16:52:08 -0700

    Add a demo website for the load balancing

---
+++ DemoWebsite/src/main/java/me/brimon/nlb/demo/controller/WebController.java
@@ -0,0 +1,26 @@
+package me.brimon.nlb.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/index")
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) throws UnknownHostException {
+        String ipAddress = Inet4Address.getLocalHost().getHostAddress();
+        model.addAttribute("ipAddress", ipAddress);
+        return "index";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
